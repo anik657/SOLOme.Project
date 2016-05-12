5515e3b8e76b6408f3bc7b5d4fee61c44b568ea4
anik657 <anik657@Phams-MacBook-Pro.local>
2016-05-12 16:40:44 -0400

    fix

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,7 +6,7 @@ var https = require('https');
 var bodyParser = require('body-parser');
 var Client = require('node-rest-client').Client;
 var client = new Client();
-var port = process.env.PORT || 8080;
+var port = 80;
 // var MatchMaker = require('../anxiliary/MatchMaking/MatchMaker.js');
 
 
